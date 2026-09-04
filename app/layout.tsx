0ceeaa64ee1d7ec488103b1aef0045dc3e9a92bb
Sara Lins <saralinstech@gmail.com>
2026-09-04 20:54:33 -0300

    Smooth hero browser card return transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import "./home-flow.css";
 import "./hero-motion.css";
 import "./hero-browser-cards.css";
 import "./hero-browser-cards-v2.css";
+import "./hero-browser-cards-v3.css";
 import "./material-icons.css";
 import "./material-icons-fix.css";
 import "./bio-carousel-label.css";
