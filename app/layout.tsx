3a5a51ad4631517978dc43f5a26c94f83fe37f89
Sara Lins <saralinstech@gmail.com>
2026-09-04 20:58:23 -0300

    Add browser controls to hero cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./hero-motion.css";
 import "./hero-browser-cards.css";
 import "./hero-browser-cards-v2.css";
 import "./hero-browser-cards-v3.css";
+import "./hero-browser-controls.css";
 import "./material-icons.css";
 import "./material-icons-fix.css";
 import "./bio-carousel-label.css";
