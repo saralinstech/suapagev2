66fd105f1414e6e83f560ba32e694b0f7601c03a
Sara Lins <saralinstech@gmail.com>
2026-09-04 20:22:09 -0300

    Style hero cards as browser screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import "./bio-accessory-icons.css";
 import "./header-polish.css";
 import "./home-flow.css";
 import "./hero-motion.css";
+import "./hero-browser-cards.css";
 import "./material-icons.css";
 import "./material-icons-fix.css";
 import "./bio-carousel-label.css";
